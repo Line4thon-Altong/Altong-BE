5044a82de9149dfa5044a9b9dd59e91b91c9090c
haejee0514 <haejee0514@sookmyung.ac.kr>
2025-11-03 17:26:42 +0900

    feat: repository 메서드 추가

--- src/main/java/com/altong/altong_backend/store/repository/StoreRepository.java
+++ src/main/java/com/altong/altong_backend/store/repository/StoreRepository.java
@@ -1,8 +1,13 @@
 package com.altong.altong_backend.store.repository;
 
+import com.altong.altong_backend.owner.model.Owner;
 import com.altong.altong_backend.store.model.Store;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface StoreRepository extends JpaRepository<Store, Long> {
     boolean existsByName(String name);
+
+    Optional<Store> findByOwner(Owner owner);
 }
