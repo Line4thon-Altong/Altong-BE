bd4735d4bf5b2f45c94f2cfdc4dfe6250c23b6c0
haejee0514 <haejee0514@sookmyung.ac.kr>
2025-11-03 17:48:46 +0900

    feat: JwtAuthFilter 수정

--- src/main/java/com/altong/altong_backend/global/jwt/JwtAuthFilter.java
+++ src/main/java/com/altong/altong_backend/global/jwt/JwtAuthFilter.java
@@ -1,9 +1,13 @@
 package com.altong.altong_backend.global.jwt;
 
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.JwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -21,7 +25,20 @@ public class JwtAuthFilter extends OncePerRequestFilter {
         String auth = req.getHeader("Authorization");
         if (StringUtils.hasText(auth) && auth.startsWith("Bearer ")) {
             String token = auth.substring(7);
-            try { provider.parse(token); } catch (Exception ignored) {}
+            try {
+                // 토큰 검증 + 인증 객체 생성 + 시큐리티 컨텍스트 등록
+                Authentication authentication = provider.getAuthentication(token);
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            } catch (ExpiredJwtException e) {
+                res.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                return;
+            } catch (JwtException e) {
+                res.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                return;
+            } catch (Exception e) {
+                res.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                return;
+            }
         }
         chain.doFilter(req,res);
     }
