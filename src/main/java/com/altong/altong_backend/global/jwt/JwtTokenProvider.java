1a50516bc2342cee3e0cef900e3df33ee957aede
haejee0514 <haejee0514@sookmyung.ac.kr>
2025-11-03 17:49:20 +0900

    feat: JwtTokenProvider 수정

--- src/main/java/com/altong/altong_backend/global/jwt/JwtTokenProvider.java
+++ src/main/java/com/altong/altong_backend/global/jwt/JwtTokenProvider.java
@@ -8,11 +8,16 @@ import io.jsonwebtoken.security.Keys;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Component;
 
 import java.nio.charset.StandardCharsets;
 import java.security.Key;
+import java.util.Collections;
 import java.util.Date;
+import java.util.List;
 
 @Component
 @RequiredArgsConstructor
@@ -58,5 +63,19 @@ public class JwtTokenProvider {
                 .setSigningKey(key)
                 .build()
                 .parseClaimsJws(token);
+    }
+
+    /** 인증 객체 생성 */
+    public Authentication getAuthentication(String token) {
+        Jws<Claims> claimsJws = parse(token);
+        Claims claims = claimsJws.getBody();
+
+        String subject = claims.getSubject();
+        String role = claims.get("role", String.class);
+
+        List<SimpleGrantedAuthority> authorities =
+                Collections.singletonList(new SimpleGrantedAuthority("ROLE_" + role));
+
+        return new UsernamePasswordAuthenticationToken(subject, null, authorities);
     }
 }
