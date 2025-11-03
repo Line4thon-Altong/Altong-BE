799f852321e94d946b1c53bdfdb09809b29e8384
haejee0514 <haejee0514@sookmyung.ac.kr>
2025-11-03 20:39:01 +0900

    feat: repository 메서드 추가

--- src/main/java/com/altong/altong_backend/employee/repository/EmployeeRepository.java
+++ src/main/java/com/altong/altong_backend/employee/repository/EmployeeRepository.java
@@ -1,11 +1,14 @@
 package com.altong.altong_backend.employee.repository;
 
 import com.altong.altong_backend.employee.model.Employee;
+import com.altong.altong_backend.store.model.Store;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
     boolean existsByUsername(String username);
     Optional<Employee> findByUsername(String username);
+    List<Employee> findByStore(Store store);
 }
