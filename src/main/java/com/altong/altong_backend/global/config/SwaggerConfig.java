2a3252c023c2126d1e7ece8a896a8ff036b57e3f
taehwan <th2194@naver.com>
2025-11-05 02:17:10 +0900

    feat: jwt 인증 및 swagger 인증 통합

--- src/main/java/com/altong/altong_backend/global/config/SwaggerConfig.java
+++ src/main/java/com/altong/altong_backend/global/config/SwaggerConfig.java
@@ -1,19 +1,50 @@
 package com.altong.altong_backend.global.config;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
+@OpenAPIDefinition(
+    info = @Info(
+        title = "Altong API",
+        version = "v1.0.0",
+        description = "알통 통합 로그인/회원가입 API 명세서"
+    ),
+    security = {
+        @SecurityRequirement(name = "bearerAuth")
+    }
+)
+@SecurityScheme(
+    name = "bearerAuth",
+    type = SecuritySchemeType.HTTP,
+    scheme = "bearer",
+    bearerFormat = "JWT"
+)
+
 public class SwaggerConfig {
 
     @Bean
     public OpenAPI altongOpenAPI() {
         return new OpenAPI()
-                .info(new Info()
-                        .title("Altong API")
-                        .description("알통 통합 로그인/회원가입 API 명세서")
-                        .version("v1.0.0"));
+            .components(new Components()
+                .addSecuritySchemes(
+                    "bearerAuth",
+                    new io.swagger.v3.oas.models.security.SecurityScheme()
+                        .type(io.swagger.v3.oas.models.security.SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT")
+                )
+            )
+            // 전역 SecurityRequirement(모델 클래스)도 풀네임으로
+            .addSecurityItem(new io.swagger.v3.oas.models.security.SecurityRequirement().addList("bearerAuth"))
+            .info(new io.swagger.v3.oas.models.info.Info()
+                .title("Altong API")
+                .version("v1.0.0")
+                .description("알통 로그인/회원가입 및 인증 테스트용 Swagger 문서"));
     }
 }
