44325a1c290a85103e1061d50260516691c82513
hyodongg <whgyehdjhd@naver.com>
2025-11-05 12:52:59 +0900

    ✨ Feat: 클래스별 파일 생성
    feat: 클래스별 파일 생성

---
+++ src/main/java/com/altong/altong_backend/schedule/employee/controller/EmployeeScheduleController.java
@@ -0,0 +1,9 @@
+package com.altong.altong_backend.schedule.employee.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+public class EmployeeScheduleController {
+}
---
+++ src/main/java/com/altong/altong_backend/schedule/employee/service/EmployeeScheduleService.java
@@ -0,0 +1,11 @@
+package com.altong.altong_backend.schedule.employee.service;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+@Builder
+public class EmployeeScheduleService {
+}
