75fc85cb8900682454ec0c5d65f09aea56976a4d
haejee0514 <haejee0514@sookmyung.ac.kr>
2025-11-08 02:13:00 +0900

    feat: repository 메서드 추가

--- src/main/java/com/altong/altong_backend/manual/repository/ManualRepository.java
+++ src/main/java/com/altong/altong_backend/manual/repository/ManualRepository.java
@@ -4,6 +4,9 @@ import com.altong.altong_backend.manual.model.Manual;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ManualRepository extends JpaRepository<Manual, Long> {
+    Optional<Manual> findByTraining_Id(Long trainingId);
 }
