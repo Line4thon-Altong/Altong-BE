a533334a2d83063e83b36be5a88a5b9e28c9db81
haejee0514 <haejee0514@sookmyung.ac.kr>
2025-11-10 13:58:26 +0900

    feat: response dto 추가

---
+++ src/main/java/com/altong/altong_backend/quiz/dto/response/QuizDetailResponse.java
@@ -0,0 +1,36 @@
+package com.altong.altong_backend.quiz.dto.response;
+
+import com.altong.altong_backend.quiz.model.Quiz;
+import com.altong.altong_backend.quiz.model.QuizType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class QuizDetailResponse {
+    private Long id;
+    private QuizType type;
+    private String question;
+    private String options;
+    private String answer;
+    private String explanation;
+    private Boolean isCompleted;
+    private String createdAt;
+
+    public static QuizDetailResponse from(Quiz quiz) {
+        return QuizDetailResponse.builder()
+                .id(quiz.getId())
+                .type(quiz.getType())
+                .question(quiz.getQuestion())
+                .options(quiz.getOptions()) // JSON 그대로 문자열로
+                .answer(quiz.getAnswer())
+                .explanation(quiz.getExplanation())
+                .isCompleted(quiz.getIsCompleted())
+                .createdAt(quiz.getCreatedAt().toString())
+                .build();
+    }
+}
