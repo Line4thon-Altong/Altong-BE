9a6c6bf479acba6f10b62675c15ea0b1ede3955d
haejee0514 <haejee0514@sookmyung.ac.kr>
2025-11-10 18:58:27 +0900

    feat: repository 수정 및 추가

--- src/main/java/com/altong/altong_backend/quiz/repository/QuizRepository.java
+++ src/main/java/com/altong/altong_backend/quiz/repository/QuizRepository.java
@@ -2,11 +2,14 @@ package com.altong.altong_backend.quiz.repository;
 
 import com.altong.altong_backend.quiz.model.Quiz;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface QuizRepository extends JpaRepository<Quiz, Long> {
-    List<Quiz> findByTraining_Id(Long trainingId);
+    @Query("SELECT q FROM Quiz q WHERE q.training.id = :trainingId ORDER BY q.id ASC")
+    List<Quiz> findByTraining_IdOrderByIdAsc(@Param("trainingId") Long trainingId);
 }
